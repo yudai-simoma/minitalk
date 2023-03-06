2b1086ff049e6a6f91ea97b2bf99e1fed8ff1edf
yudai-shimoma <yshimoma@student.42tokyo.jp>
2023-03-06 17:45:10 +0900

    bitデータ送信できるようにした

--- client.c
+++ client.c
@@ -6,15 +6,29 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 11:35:55 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/03/04 12:36:27 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:41:44 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(int ac, char **av)
+int	main(int argc, char *argv[])
 {
-	(void)ac;
-	kill((pid_t)atoi(av[1]), SIGUSR1);
+	int		i;
+	char	c;
+
+	(void)argc;
+	i = 0;
+	while (*argv != '\0')
+	{
+		while (i < 8)
+		{
+			c = 1 << i;//iの数だけ1をシフトするため1で固定
+			c = *argv[2] & c;
+			kill((pid_t)atoi(argv[1]), SIGUSR1 + (c >> i));
+			i++;
+			argv[2]++;
+		}
+	}
 	return (0);
 }
