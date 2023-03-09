997b1173343881b4a30ed290a80457745327c4cb
yudai-shimoma <yshimoma@student.42tokyo.jp>
2023-03-09 21:55:42 +0900

    error処理追加

--- client.c
+++ client.c
@@ -6,18 +6,42 @@
 /*   By: yshimoma <yshimoma@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 11:35:55 by yshimoma          #+#    #+#             */
-/*   Updated: 2023/03/09 20:17:57 by yshimoma         ###   ########.fr       */
+/*   Updated: 2023/03/09 21:55:01 by yshimoma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
 /*
+ * プロセスIDが数値だけかのチェック
+ */
+int	ft_num_check(char *a_argv)
+{
+	int	i;
+
+	i = 0;
+	while (a_argv[i] != '\0')
+	{
+		if (!ft_isdigit(a_argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/*
  * コマンドライン引数のエラーチェック
  */
-int	ft_error_check(int a_argc)
+int	ft_error_check(int a_argc, char **a_argv)
 {
+	int	num;
+
 	if (a_argc != 3)
+		return (1);
+	if (ft_num_check(a_argv[1]))
+		return (1);
+	num = ft_atoi(a_argv[1]);
+	if (num < 0 || num > 99999)
 		return (1);
 	return (0);
 }
@@ -45,29 +69,28 @@ static void	ft_conversion_bit_2(int a_num, char a_bit_str[9])
 		i++;
 		j--;
 	}
-	ft_printf("bitstr = %s", a_bit_str);
 }
 
 /*
  * serverへ文字列を送るプログラム（正常に送れたらserverからもシグナルを受け取る）
- * コマンドライン引数：第二引数　プロセスID
- * 　　　　　　　　　　第三引数　sarverへ送りたい文字列
+ * コマンドライン引数 : 第二引数 プロセスID
+ * 					 第三引数 sarverへ送りたい文字列
  * 文字列を一文字ずつ送信、1文字を8ビットのバイナリデータで送信する
  * 一文字ずつft_conversion_bit2の関数へ渡し、バイナリデータを作成
  * 
  * usleepは450に設定、一秒間に約277文字遅れる設定にしている
  * 計算方法
  * 	1秒は1000000マイクロ秒 
- * 	100 * 8 = 8000マイクロ秒
- * 	1000000 / 800 = 1250
+ * 	450 * 8 = 3600マイクロ秒
+ * 	1000000 / 3600 = 277
  */
-int	main(int a_argc, char **a_argv)
+int	main(int a_argc, char *a_argv[])
 {
 	int		i;
 	int		j;
 	char	r_bit_str[9];
 
-	if (ft_error_check(a_argc) == 1)
+	if (ft_error_check(a_argc, a_argv))
 		return (0);
 	i = 0;
 	r_bit_str[8] = '\0';
@@ -82,26 +105,12 @@ int	main(int a_argc, char **a_argv)
 			else if (r_bit_str[j] == '0')
 				kill((pid_t)ft_atoi(a_argv[1]), SIGUSR2);
 			j++;
-			usleep(100);
+			usleep(450);
 		}
 		i++;
 	}
 	return (0);
 }
-
-//エラー処理
-//serverの改行する必要がある？
-//改行はいらない
-
-//exit関数の使用有無
-//killのエラー処理が必要、exitを使う
-
-//ボーナスの以下は一文字づつ返す？文字列全部送られたら1回返す？
-//	サーバーは受信した全てのメッセージに対して、クライアントに信号を送り返すことで承認する。
-
-//serverからのシグナル受け取り
-//client側へのシグナル送信
-
 
 /*
  * 実行方法
@@ -109,3 +118,5 @@ int	main(int a_argc, char **a_argv)
  * ./client プロセスID 送りたい文字
  * ./client 7392 abcdefghijk
  */
+
+//ボーナスは1シグナルを受け取るごとにcrientに返すよう実装
